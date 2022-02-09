d432c34f7fcafe376a155dfde1e4dd1c1a7f1c55
Will Archer Arentz <willarentz@gmail.com>
2022-02-09 11:58:31 +0100

    get up to speed

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+mkdir $HOME/.config
+ln -s `pwd`/.vim $HOME/.vim
+ln -s `pwd`/.vimrc $HOME/.vimrc
+ln -s `pwd`/.tmux.conf $HOME/.tmux.conf
+ln -s `pwd`/.tmux.powerline.conf $HOME/.tmux.powerline.conf
+ln -s `pwd`/.config/fish $HOME/.config/fish
+ln -s `pwd`/.config/nvim $HOME/.config/nvim
+ln -s `pwd`/.config/peco $HOME/.config/peco
+ln -s `pwd`/.config/tmux $HOME/.config/tmux
+ln -s `pwd`/.config/vim $HOME/.config/vim
